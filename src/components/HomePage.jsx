125d881b006ad8e337572fac4c1a21fb5c26717b
RhogeDiaz <rvadiaz09@gmail.com>
2024-12-26 18:51:31 +0800

    update home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div className=' h-4/6 pt-48 pb-48'>
-      <div className='flex w-full justify-center items-center'>Contents coming soon!</div>
-      
+      <div className='flex w-full justify-center items-center'>
+        <p className=' text-4xl font-bold font-helvetica'>THE <p className=' text-[#dac770]'>SATELLITE</p> INTERNET</p>
+      </div>
     </div>    
   )
 }
