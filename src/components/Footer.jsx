eb017d8802813081ca74e1eae22620f81163594b
RhogeDiaz <rvadiaz09@gmail.com>
2024-12-26 23:20:22 +0800

    fixed footer sizing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <div className="p-4 flex justify-between border-t mt-44 h-20">
+        <div className="p-4 flex justify-between border-t mt-44">
             <div className="self-start flex flex-col justify-center">
                 <h1 className="font-bold text-md">Rhoge Vhir A. Diaz</h1>
                 <div className="flex items-center gap-2 text-sm">December 2024</div>
